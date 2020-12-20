bfe43f331b51c92c4f7a0aca5e266066eefefc78
Mufassir Ahmad Chowdhury <mac22214u@gmail.com>
2020-12-20 08:36:37 +0600

    backup

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+<div class="relative max-w-full h-16 flex nav shadow-lg">
+<span class="text-4xl text-center my-auto mx-6">Twotter</span>
+<span class="absolute user">@MAC</span>
+</div>
 <div id="app"  class="overflow-x-scroll">
   <user-profile/>
 </div>
@@ -20,6 +24,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   min-height: 100vh;
+  background-color: #F3F5FA;
+}
+.user {
+  right:5%;
+  top:25%;
+  text-align: center;
+}
+.nav {
   background-color: #F3F5FA;
 }
 </style>
